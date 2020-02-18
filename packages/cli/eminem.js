843326c2f4e4a5e0198e7fd604f9bc0ab5d3886a
wyyufeng <wyyufeng@gmail.com>
2020-02-18 18:21:48 +0800

    重写了cli

---
+++ packages/cli/eminem.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+const path = require("path");
+const validAppName = require("validate-npm-package-name");
+const chalk = require("chalk");
+const fs = require("fs-extra");
+const inquirer = require("inquirer");
+const InitTask = require("./tasks/Init");
+const { info, error, warn } = require("./tasks/util");
+process.on("unhandledRejection", err => {
+  throw err;
+});
+const program = new commander.Command("eminem");
+
+let projectName;
+
+program
+  .command("init <project-directory>")
+  .description("初始化项目")
+  .option("--template <template>", "模板名称")
+  .option("--useYarn", "是否使用yarn作为包管理器")
+  .option("--usecnpm", "使用淘宝源")
+  .action((name, options) => {
+    console.log(options.useYarn, options.usecnpm);
+    projectName = name;
+    const projectDir = path.resolve(process.cwd(), projectName);
+    const isCurrentDir = projectName === ".";
+    validateName(name, isCurrentDir);
+    checkProjectDir(isCurrentDir, projectDir).then(() => {
+      new InitTask(name, options.template, options.useYarn).createApp();
+    });
+  });
+
+program.parse(process.argv);
+
+function validateName(projectName, isCurrentDir) {
+  const name = isCurrentDir ? path.relative("../", process.cwd()) : projectName;
+  const result = validAppName(name);
+  if (result.errors) {
+    result.errors.forEach(err => {
+      console.log(chalk.red.dim("Error: " + err));
+      process.exit(1);
+    });
+  }
+}
+
+async function checkProjectDir(isCurrentDir, projectDir) {
+  if (fs.existsSync(projectDir)) {
+    if (isCurrentDir) {
+      const { ok } = await inquirer.prompt([
+        {
+          name: "ok",
+          type: "confirm",
+          message: `是否在当前目录下创建项目?`
+        }
+      ]);
+      if (!ok) return process.exit(1);
+    } else {
+      const { ok } = await inquirer.prompt([
+        {
+          name: "ok",
+          type: "confirm",
+          message: `目录 ${chalk.cyan(projectDir)} 已经存在，是否覆盖?`
+        }
+      ]);
+      if (!ok) {
+        warn("退出拉倒!");
+        return process.exit(1);
+      }
+      if (ok) {
+        info(`正在删除 ${chalk.cyan(projectDir)}...`);
+
+        // 为啥有时候remove会失败呢
+        try {
+          fs.removeSync(projectDir);
+          if (fs.existsSync(projectDir)) {
+            fs.emptyDirSync(projectDir);
+          } else {
+            fs.mkdirSync(projectDir);
+          }
+        } catch (e) {
+          error("删除文件时出现异常，请重试！");
+          process.stdout.write("\n");
+          console.error(e);
+          process.exit(1);
+        }
+      }
+    }
+  } else {
+    fs.mkdirSync(projectDir);
+  }
+}
---
+++ packages/cli/tasks/util.js
@@ -0,0 +1,43 @@
+const logSymbols = require("log-symbols");
+const spawn = require("cross-spawn");
+function info(msg) {
+  console.log(logSymbols.info, msg);
+}
+
+function success(msg) {
+  console.log(logSymbols.success, msg);
+}
+function error(msg) {
+  console.log(logSymbols.error, msg);
+}
+function warn(msg) {
+  console.log(logSymbols.warning, msg);
+}
+function installPkg(deps = [], command = "npm") {
+  return new Promise((resolve, reject) => {
+    const args = [];
+    if (command === "npm") {
+      args.push("install");
+    }
+    if (command === "yarn") {
+      args.push("add");
+    }
+    args.push(...deps);
+    spawn("npm", [...args], {
+      stdio: "inherit"
+    }).on("close", code => {
+      if (code !== 0) {
+        reject();
+        return;
+      }
+      resolve();
+    });
+  });
+}
+module.exports = {
+  info,
+  success,
+  error,
+  warn,
+  installPkg
+};
