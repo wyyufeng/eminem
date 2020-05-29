aa14841048a9cecffb533f4668822d5770316f09
wyyufeng <wyyufeng@gmail.com>
2020-05-29 16:54:23 +0800

    优化信息提示

--- packages/scripts/commands/build.js
+++ packages/scripts/commands/build.js
@@ -58,7 +58,13 @@ function build() {
                 replacer: null
             }
         );
-        console.log(`You can view the result with:  ${chalk.blueBright('npx serve build')}`);
+        console.log(`You can view the result with: `);
+        console.log(`${chalk.blueBright('npx serve build')}`);
+        console.log(
+            `You can also install the package globally using Yarn with:   ${chalk.blueBright(
+                'yarn global add serve'
+            )},and then run: ${chalk.blueBright('serve build')}`
+        );
     });
 }
 
